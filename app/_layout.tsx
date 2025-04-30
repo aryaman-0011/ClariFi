cb8f63527ca3d0aec2c01342e429f4054829f00c
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-04-30 13:20:12 +0530

    Set header to false

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 import { Stack } from 'expo-router'
 
 const _layout = () => {
-  return (
-    <Stack></Stack>
-  )
+    return (
+        <Stack screenOptions={{ headerShown: false }}></Stack>
+    )
 }
 
 export default _layout
