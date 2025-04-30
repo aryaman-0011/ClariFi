ab82204b6d100d6dbda28d6e429f9cbf3dc95576
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-04-30 13:20:28 +0530

    Added route to welcome page

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { colors } from '@/constants/theme'
+import { useRouter } from 'expo-router'
 
 const index = () => {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/(auth)/welcome')
+        }, 2000)
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <StatusBar hidden={true} />
+            <Image style={styles.logo} resizeMode='contain' source={require('../assets/images/splashImage.png')} />
+        </View>
+    )
 }
 
 export default index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.neutral900,
+    },
+
+    logo: {
+        height: '20%',
+        aspectRatio: 1,
+    }
+})
