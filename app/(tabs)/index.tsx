45a252b1056647edf47698a5d53abf31776efe85
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-05-02 13:15:56 +0530

    Added logout functionality for testing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Button from '@/components/Button'
+import Typo from '@/components/Typo'
+import { colors } from '@/constants/theme'
+import { auth } from '@/config/firebase'
+import { signOut } from 'firebase/auth'
+import { useAuth } from '@/contexts/authContext'
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+  const {user} = useAuth()
+
+  console.log("user: ", user)
+
+  const handleLogout = async () => {
+  await signOut(auth)
+}
+
+return (
+  <View>
+    <Text>Home</Text>
+    <Button onPress={handleLogout}>
+      <Typo color={colors.black}>Logout</Typo>
+    </Button>
+  </View>
+)
 }
 
 export default Home
