6fefb6a905ac24bb7726c812e46e2652d83338f9
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-05-10 02:35:53 +0530

    Delete wallet from database

--- services/walletService.ts
+++ services/walletService.ts
@@ -1,7 +1,7 @@
 import { ResponseType, WalletType } from "@/types";
 import { uploadFileToCloudinary } from "./imageService";
 import { firestore } from "@/config/firebase";
-import { collection, doc, setDoc } from "firebase/firestore";
+import { collection, deleteDoc, doc, setDoc } from "firebase/firestore";
 
 export const createOrUpdateWallet = async (
     walletData: Partial<WalletType>
@@ -36,5 +36,21 @@ export const createOrUpdateWallet = async (
     } catch (error: any) {
         console.log('Error creating or updating wallet', error)
         return { success: false, msg: error.message }
+    }
+}
+
+
+export const deleteWallet = async (walletId: string): Promise<ResponseType> => {
+    try {
+        const walletref = doc(firestore, "wallets", walletId)
+        await deleteDoc(walletref)
+
+        // Todo: delete all transactions related to this doc
+
+        return {success: true, msg: "Wallet deleted successfully"}
+
+    } catch (err: any) {
+        console.log('Error deleting the wallet: ', err)
+        return { success: false, msg: err.message }
     }
 }
