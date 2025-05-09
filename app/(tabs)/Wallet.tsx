8405362bf02d54b9fccdeaa0a364831c74d75241
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-05-09 22:24:28 +0530

    Added Wallet list component

--- app/(tabs)/Wallet.tsx
+++ app/(tabs)/Wallet.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper'
 import { colors, radius, spacingX, spacingY } from '@/constants/theme'
@@ -6,10 +6,23 @@ import { verticalScale } from '@/utils/styling'
 import Typo from '@/components/Typo'
 import * as Icons from 'phosphor-react-native'
 import { useRouter } from 'expo-router'
+import useFetchData from '@/hooks/useFetchData'
+import { WalletType } from '@/types'
+import { orderBy, where } from 'firebase/firestore'
+import { useAuth } from '@/contexts/authContext'
+import Loading from '@/components/Loading'
+import index from '..'
+import WalletListItem from '@/components/WalletListItem'
 
 const Wallet = () => {
 
   const router = useRouter()
+  const { user } = useAuth()
+
+  const { data: wallets, error, loading } = useFetchData<WalletType>("wallets", [
+    where("uid", "==", user?.uid),
+    orderBy("created", "desc")
+  ])
 
   const getTotalBalance = () => {
     return 20000
@@ -46,6 +59,15 @@ const Wallet = () => {
           </View>
 
           {/* Wallet List */}
+          {loading && <Loading />}
+          <FlatList
+            data={wallets}
+            renderItem={({ item, index }) => {
+              return <WalletListItem item={item} index={index} router={router}
+              />
+            }}
+            contentContainerStyle = {styles.listStyle}
+          />
 
 
         </View>
