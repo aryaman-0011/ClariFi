68c80a570d4e789999fe5bd1b125e6066a40f033
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-07-06 23:58:42 +0530

    Backend Setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT
+
+app.get("/", (req, res) => {
+    res.send("It's working")
+})
+
+console.log("My Port:",process.env.PORT)
+
+app.listen(PORT, () => {
+    console.log("Server is up and running on PORT: ", PORT)
+})
